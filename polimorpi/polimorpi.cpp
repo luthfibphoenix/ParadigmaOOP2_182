1ce097ad78c9be31193a347268f4dea29c65c7b0
luthfibphoenix <lutfibahtiar2409@gmail.com>
2023-05-29 15:55:51 +0700

    26 int main()

--- polimorpi/polimorpi.cpp
+++ polimorpi/polimorpi.cpp
@@ -22,3 +22,16 @@ public:
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
+int main() {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+	//b.seseorang::pesan();
+}
